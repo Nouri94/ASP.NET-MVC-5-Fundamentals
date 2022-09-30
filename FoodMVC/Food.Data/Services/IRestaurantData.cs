b8989101487dedd6fea3d5a2a6e2089783e82705
Issaki <nouri.issaki@capgemini.com>
2022-09-30 14:57:18 +0200

    Create a New ASP:NET MVC Application

---
+++ FoodMVC/Food.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using Food.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
